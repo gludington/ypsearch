4cb4b2bef8fcb544027edf8c8f814621415825c1
Greg Ludington <Greg.Ludington@ipsoft.com>
2023-06-12 21:34:25 -0400

    a very quick and dirty search

---
+++ src/app/api/sessions/route.ts
@@ -0,0 +1,80 @@
+import { NextResponse } from "next/server";
+
+type VddwSession = {
+    title: string;
+    code?: string;
+    dm?: string;
+    vtt?: string;
+    url: string;
+    description: string;
+    startDate: number;
+}
+
+type VddwSessionResponse = {
+    results: VddwSession[];
+}
+
+const toVddwSession = (session: any): VddwSession => {
+    // title
+    // Table #1 - BMG-MOON-MD-04 - Lair of Deceit - Owlbear.Rodeo
+    // Red Carpet Booking - Your Adventures - We Pick the DM
+    const titleLine = session.title.split(' - ')
+    let isRedCarpet: boolean;
+    let code: string | null = null;
+    let dm: string | null = null;
+    let vtt: string | null = null;
+    if (titleLine.length > 0) {
+        //does title contain Table #?
+        if (titleLine[0].indexOf("Table #") > -1) {
+            isRedCarpet = false;
+            code = titleLine[1];
+            
+            //vtt is always last
+            const vttCandidate = titleLine[titleLine.length - 1];
+            vtt = vttCandidate;
+        } else {
+            isRedCarpet = true;
+        }
+    } else {
+        isRedCarpet = true;
+        code = null;
+        dm = null;
+        vtt = null;
+    }
+
+    const shortDescriptionLine = session.description_short.split(' - ');
+    if (shortDescriptionLine.length > 0) {
+        const dmSegment = shortDescriptionLine.find(desc => desc.indexOf("DM: ") > -1)
+        dm = dmSegment ? dmSegment.substring("DM :".length) : undefined;
+    }
+
+    return {
+        title: session.title,
+        code: code || undefined,
+        dm: dm || undefined,
+        vtt: vtt || undefined,
+        url: session.url,
+        description: session.description,
+        startDate: isRedCarpet ? 0 :session.start_date ? Date.parse(session.start_date) : 0
+    };
+}
+
+const url = "https://yawningportal.dnd.wizards.com/api/event?chapter=26&page_size=900&status=Live&include_cohosted_events=true&visible_on_parent_chapter_only=true&order_by=start_date&fields=title,start_date,event_type_title,cropped_picture_url,cropped_banner_url,url,cohost_registration_url,description,description_short&page=1";
+export async function GET() {
+    const res = await fetch(url);
+    const data = await res.json();
+    let results: VddwSession[];
+    if (data?.results?.length) {
+        results = data.results.map(toVddwSession);
+    } else {
+        results = [];
+    }
+  
+  return NextResponse.json({ results: results },     {
+      status: 200,
+      headers: {
+        'content-type': 'application/json',
+        'cache-control': 'public, s-maxage=1200, stale-while-revalidate=600',
+      },
+    },)
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,203 @@
-import Image from 'next/image'
+"use client"
+import axios from 'axios';
+import { useEffect, useState, Fragment } from 'react';
+import { Menu, Transition } from '@headlessui/react'
+import { ChevronDownIcon } from '@heroicons/react/20/solid'
 
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ')
+}
+
+
+const dateString = (date: Date) => {
+  return date ? date.toLocaleString(navigator.language || 'en-us', { weekday: "long", month: "short", day: "numeric", hour: "numeric", minute: "numeric" }) : "";
+}
+
+const filterToString = (filter: { time: number, vtt: string, dm: string }) =>  {
+  if (filter.time || filter.vtt || filter.dm) {
+    const ret = [];
+    if (filter.time) {
+        ret.push(`at ${dateString(new Date(filter.time))}`)
+    }
+    if (filter.dm) {
+      ret.push(`with ${filter.dm}`)
+    }
+    if (filter.vtt) {
+      ret.push(`on ${filter.vtt}`)
+    }
+    return ret.join(", ");
+  } else {
+    return ""
+  }
+}
+
+function Dropdown({ title, items =[], onSelect }: { title:string, items:{value:string, text: string}[], onSelect: (value: any) => void}) {
+  return (
+    <Menu as="div" className="relative inline-block text-left">
+      <div>
+        <Menu.Button className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
+          {title}
+          <ChevronDownIcon className="-mr-1 h-5 w-5 text-gray-400" aria-hidden="true" />
+        </Menu.Button>
+      </div>
+
+      <Transition
+        as={Fragment}
+        enter="transition ease-out duration-100"
+        enterFrom="transform opacity-0 scale-95"
+        enterTo="transform opacity-100 scale-100"
+        leave="transition ease-in duration-75"
+        leaveFrom="transform opacity-100 scale-100"
+        leaveTo="transform opacity-0 scale-95"
+      >
+        <Menu.Items className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+          <div className="py-1">
+            <Menu.Item key={"ANY"}>
+                {({ active }) => (
+                  <a
+                    onClick={(evt) => onSelect(null)}
+                    className={classNames(
+                      active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                      'block px-4 py-2 text-sm'
+                    )}
+                  >
+                    Any
+                  </a>
+                )}
+              </Menu.Item>
+            {items.map(item => (
+              <Menu.Item key={item.value}>
+                {({ active }) => (
+                  <a
+                    onClick={(evt) => onSelect(item.value)}
+                    className={classNames(
+                      active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                      'block px-4 py-2 text-sm'
+                    )}
+                  >
+                    {item.text}
+                  </a>
+                )}
+              </Menu.Item>
+            ))}
+          </div>
+        </Menu.Items>
+      </Transition>
+    </Menu>
+  )
+}
 export default function Home() {
+  const [sessions, setSessions] = useState([]);
+  const [dms, setDms] = useState([])
+  const [vtts, setVtts] = useState([]);
+  const [times, setTimes] = useState([]);
+  const [filter, setFilter] = useState({ time: null, dm: null, vtt: null });
+  const [filteredResults, setFilteredResults] = useState([]);
+  
+  useEffect(() => {
+    axios.get('/api/sessions').then(rsp => {
+      let newResults = [];
+      let newDms = new Set();
+      let newVtts = new Set();
+      let newTimes = new Set();
+      rsp.data.results.forEach(session => {
+        const sessionDate = session.startDate ? new Date(session.startDate) : null
+        newResults.push({ ...session, startDate: sessionDate });
+        if (session.dm) {
+          newDms.add(session.dm);
+        }
+        newVtts.add(session.vtt || 'Unknown');
+        newTimes.add(session.startDate);
+      })
+      setSessions(newResults);
+      setFilteredResults(newResults);
+      setTimes(Array.from(newTimes).sort().map(time => { return { value: time || 0, text: time ? dateString(new Date(time)) : "No Time" } }));
+      setDms(Array.from(newDms).sort().map(dm => { return { value: dm, text: dm } }));
+      setVtts(Array.from(newVtts).sort().map(vtt => { return { value: vtt, text: vtt } }));
+    })
+  }, []);
+  useEffect(() => {
+    if (filter.time || filter.dm || filter.vtt) {
+      let results = sessions;
+      if (filter.time) {
+        results = results.filter(session => {
+          return session.startDate ? filter.time === session.startDate.getTime() : false;
+        })
+      }
+      if (filter.dm) {
+        results = results.filter(session => {
+          return session.dm ? filter.dm === session.dm : false;
+        });
+      }
+      if (filter.vtt) {
+        results = results.filter(session => {
+          return session.vtt ? filter.vtt === session.vtt : false;
+        });
+      }
+      setFilteredResults(results || []);
+    } else {
+      setFilteredResults(sessions);
+    }
+  }, [ filter, sessions])
+  if (!sessions) {
+    return <h2>Loading...</h2>
+  }
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="px-4 sm:px-6 lg:px-8">
+      <div className="sm:flex sm:items-center">
+        <div className="sm:flex-auto">
+          <h1 className="text-base font-semibold leading-6 text-gray-900">Sessions at VDDW</h1>
+        </div>
+        </div>
+        <div className="flex gap-4">
+          <Dropdown title="Start Time" items={times} onSelect={(value) => setFilter((prev) => { return { ...prev, time: value } })} />
+          <Dropdown title="DM" items={dms} onSelect={(value) => setFilter((prev) => { return { ...prev, dm: value } })} />
+          <Dropdown title="VTT" items={vtts} onSelect={(value) => setFilter((prev) => { return { ...prev, vtt: value } })} />
+        </div>
+        <div className="sm:flex-auto">
+          <h1 className="text-base font-semibold leading-6 text-gray-900">Filters: {filterToString(filter)}</h1>
+        </div>
+        <div>
+        </div>
+      <div className="mt-8 flow-root">
+        <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+          <div className="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
+            <table className="min-w-full divide-y divide-gray-300">
+              <thead>
+                <tr>
+                  <th scope="col" className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-0">
+                    Name
+                  </th>
+                  <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                    Start
+                  </th>
+                  <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                    DM
+                  </th>
+                  <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                    VTT
+                  </th>
+                </tr>
+              </thead>
+              <tbody className="divide-y divide-gray-200">
+                {filteredResults.map((session, idx) => (
+                  <tr key={`${session.name}-${idx}`}>
+                    <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-0">
+                      <a className="text-indigo-600 hover:text-indigo-900" href={session.url} target="_new">{session.title}</a>
+                    </td>
+                    <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{dateString(session.startDate)}</td>
+                    <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{session.dm}</td>
+                    <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{session.vtt}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+    </div>
     </main>
   )
 }
