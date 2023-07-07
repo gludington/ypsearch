9d88bf759937b4d5628f1ceea2946923ec15db56
Greg Ludington <Greg.Ludington@ipsoft.com>
2023-07-07 18:45:40 -0400

    add question and moal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 "use client"
 import axios from 'axios';
 import { useEffect, useState, Fragment, JSXElementConstructor, PromiseLikeOfReactNode, ReactElement, ReactNode, ReactPortal } from 'react';
-import { Menu, Transition } from '@headlessui/react'
-import { ChevronDownIcon } from '@heroicons/react/20/solid'
+import { Dialog, Menu, Transition } from '@headlessui/react'
+import { ChevronDownIcon, QuestionMarkCircleIcon } from '@heroicons/react/20/solid'
 import { VddwSession } from './api/sessions/route';
 
 type ClientVddwSession = VddwSession & { sessionDate: Date }
@@ -142,6 +142,8 @@ export default function Home() {
     })
   }, []);
 
+  const [showModal, setShowModal] = useState(false);
+
   useEffect(() => {
     if (filter.time || filter.time === 0 || filter.dm || filter.vtt || filter.name || filter.tag || filter.hideSoldOut) {
       let results = sessions;
@@ -187,11 +189,75 @@ export default function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
+      {showModal ? (
+        <Transition.Root show={showModal} as={Fragment}>
+      <Dialog as="div" className="relative z-10" onClose={() => setShowModal(false)}>
+        <Transition.Child
+          as={Fragment}
+          enter="ease-out duration-300"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+        </Transition.Child>
+
+        <div className="fixed inset-0 z-10 overflow-y-auto">
+          <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+              enterTo="opacity-100 translate-y-0 sm:scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+              leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+            >
+              <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-sm sm:p-6">
+                <div>
+                  <div className="mx-auto flex h-12 w-12 items-center justify-center rounded-full bg-green-100">
+                    <img src="/d20.svg" alt="d20"/>
+                  </div>
+                  <div className="mt-3 sm:mt-5">
+                    <Dialog.Title as="h3" className="text-base text-center font-semibold leading-6 text-gray-900">
+                      VDDW Fan Site
+                    </Dialog.Title>
+                    <div className="mt-2">
+                      <p className="text-sm mb-4 text-gray-500">
+                            All data belongs to Baldman Games, and is fetched on a schedule to be a good citizen of this gaming community.
+                            Some data may therefore be out of date until the next refresh; try refresing your page.
+                      </p>
+                      <p className="text-sm text-gray-500">This is a fan site created exclusively to help people find games at Baldman events,
+                          and may be removed at their request</p>
+                    </div>
+                  </div>
+                </div>
+                <div className="mt-5 sm:mt-6">
+                  <button
+                    type="button"
+                    className="inline-flex w-full justify-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                    onClick={() => setShowModal(false)}
+                  >
+                    Back to the search
+                  </button>
+                </div>
+              </Dialog.Panel>
+            </Transition.Child>
+          </div>
+        </div>
+      </Dialog>
+    </Transition.Root>
+  ) : null}
     <div className="px-4 sm:px-6 lg:px-8">
       <div className="sm:flex sm:items-center">
-        <div className="sm:flex-auto">
-            <h1 className="text-sm font-semibold leading-6 text-gray-900">{fetchDate ? `${sessions?.length ? `${sessions.length} ` : " "}VDDW Sessions (Fetched ${shortDateString(fetchDate)})` : 'Loading...'}</h1>
-        </div>
+          {fetchDate ? (
+            <div className="flex flex-nowrap gap-4">
+              <h1 className="text-sm font-semibold leading-6 text-gray-900 whitespace-nowrap">{sessions?.length ? `${sessions.length} ` : " "}VDDW Sessions (Fetched {shortDateString(fetchDate)})</h1>
+              <QuestionMarkCircleIcon className="text-gray-900 h-4 w-4" onClick={() => setShowModal(true)}/>
+            </div>
+            ) : <div className="text-sm font-semibold leading-6 text-gray-900 whitespace-nowrap">Loading...</div>}
         </div>
         <div className="flex flex-wrap gap-4">
           <Dropdown title="Group" items={tags} onSelect={(value) => setFilter((prev) => { return { ...prev, tag: value } })} />
