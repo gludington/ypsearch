cae9ba123540949a07e4e047624551a283302d03
Greg Ludington <Greg.Ludington@ipsoft.com>
2023-11-18 11:40:41 -0500

    use combobox for autocomplete

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 "use client"
 import axios from 'axios';
 import { useEffect, useState, Fragment, useMemo } from 'react';
-import { Dialog, Menu, Transition } from '@headlessui/react'
-import { ChevronDownIcon, QuestionMarkCircleIcon } from '@heroicons/react/20/solid'
+import { Dialog, Menu, Transition, Combobox } from '@headlessui/react'
+import { CheckIcon, ChevronDownIcon, QuestionMarkCircleIcon } from '@heroicons/react/20/solid'
 import { VddwSession } from './api/sessions/route';
 import { useRouter, usePathname, useSearchParams } from 'next/navigation';
 import Skeleton from 'react-loading-skeleton';
@@ -110,58 +110,82 @@ const filterToString = (filter: FilterType) =>  {
 }
 
 function Dropdown({ title, items =[], onSelect }: { title:string, items:{value:string | number, text: string}[], onSelect: (value: any) => void}) {
+  const [selected, setSelected] = useState({ value: "", text:  `${title} (Any)` })
+  const [query, setQuery] = useState('');
+  const filteredPeople =
+    query === ''
+      ? [{ value: "", text: `${title} (Any)` }, ...items]
+      : items.filter((item) => {
+          return item.text.toLowerCase().includes(query)
+      })
+  
   return (
-    <Menu as="div" className="relative inline-block text-left">
-      <div>
-        <Menu.Button className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
-          {title}
-          <ChevronDownIcon className="-mr-1 h-5 w-5 text-gray-400" aria-hidden="true" />
-        </Menu.Button>
-      </div>
-
-      <Transition
-        as={Fragment}
-        enter="transition ease-out duration-100"
-        enterFrom="transform opacity-0 scale-95"
-        enterTo="transform opacity-100 scale-100"
-        leave="transition ease-in duration-75"
-        leaveFrom="transform opacity-100 scale-100"
-        leaveTo="transform opacity-0 scale-95"
-      >
-        <Menu.Items className="absolute left-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-          <div className="py-1">
-            <Menu.Item key={"ANY"}>
-                {({ active }) => (
-                  <a
-                    onClick={(evt) => onSelect(null)}
-                    className={classNames(
-                      active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                      'block px-4 py-2 text-sm'
-                    )}
-                  >
-                    Any
-                  </a>
-                )}
-              </Menu.Item>
-            {items.map(item => (
-              <Menu.Item key={item.value}>
-                {({ active }) => (
-                  <a
-                    onClick={(evt) => onSelect(item.value)}
-                    className={classNames(
-                      active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                      'block px-4 py-2 text-sm'
-                    )}
-                  >
-                    {item.text}
-                  </a>
-                )}
-              </Menu.Item>
-            ))}
+      <div className="relative inline-block text-left w-24">
+        <Combobox value={selected} onChange={(item) => { setSelected(item); onSelect(item.value) }}>
+        <div className="relative mt-1">
+          <div className="relative cursor-default overflow-hidden rounded-lg bg-white text-left shadow-md focus:outline-none focus-visible:ring-2 focus-visible:ring-white/75 focus-visible:ring-offset-2 focus-visible:ring-offset-indigo-300 sm:text-sm">
+            <Combobox.Input
+              className="w-full border-none py-2 pl-3 pr-10 text-sm leading-5 text-gray-900 focus:ring-0"
+              displayValue={(person) => person.text}
+              onChange={(event) => setQuery(event.target.value)}
+            />
+            <Combobox.Button className="absolute inset-y-0 right-0 flex items-center pr-2">
+              <ChevronDownIcon
+                className="h-5 w-5 text-gray-400"
+                aria-hidden="true"
+              />
+            </Combobox.Button>
           </div>
-        </Menu.Items>
-      </Transition>
-    </Menu>
+          <Transition
+            as={Fragment}
+            leave="transition ease-in duration-100"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+            afterLeave={() => setQuery('')}
+          >
+            <Combobox.Options className="absolute left-0 z-10 w-56 origin-top-right rounded-md bg-white text-base shadow-lg ring-1 ring-black/5 focus:outline-none sm:text-sm">
+              {filteredPeople.length === 0 && query !== '' ? (
+                <div className="relative cursor-default select-none px-4 py-2 text-gray-700">
+                  Nothing found.
+                </div>
+                ) : (
+                    <>
+                      {filteredPeople.map((person) => (
+                        <Combobox.Option
+                          key={person.value}
+                          className={({ active }) =>
+                            `relative cursor-default select-none py-2 pl-10 pr-4 ${active ? 'bg-indigo-600 text-white' : 'text-gray-900'
+                            }`
+                          }
+                          value={person}
+                        >
+                          {({ selected, active }) => (
+                            <>
+                              <span
+                                className={`block truncate ${selected ? 'font-medium' : 'font-normal'
+                                  }`}
+                              >
+                                {person.text}
+                              </span>
+                              {selected ? (
+                                <span
+                                  className={`absolute inset-y-0 left-0 flex items-center pl-3 ${active ? 'text-white' : 'text-indigo-600'
+                                    }`}
+                                >
+                                  <CheckIcon className="h-5 w-5" aria-hidden="true" />
+                                </span>
+                              ) : null}
+                            </>
+                          )}
+                        </Combobox.Option>
+                      ))}
+                      </>
+              )}
+            </Combobox.Options>
+          </Transition>
+        </div>
+      </Combobox>
+    </div>
   )
 }
 const emptyResult: ClientVddwSession = {
